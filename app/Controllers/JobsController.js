c3a371b339bcfa5265f36b17872d423bce2bc1f6
jon-cron <jonathanacm95@gmail.com>
2022-11-15 15:54:04 -0700

    got jobs

--- app/Controllers/JobsController.js
+++ app/Controllers/JobsController.js
@@ -1,4 +1,5 @@
 import { jobsService } from "../Services/JobsService.js";
+import { Pop } from "../Utils/Pop.js";
 
 function _drawJobs() {
   console.log("draws are connected");
@@ -12,7 +13,16 @@ export class JobsController {
   constructor() {
     console.log("controller is working");
   }
+  async getJobs() {
+    try {
+      await jobsService.getJobs();
+    } catch (error) {
+      Pop.toast("error", error);
+      console.log(error);
+    }
+  }
   showJobs() {
+    this.getJobs();
     _drawJobs();
     _drawJobsForm();
   }
--- app/Services/JobsService.js
+++ app/Services/JobsService.js
@@ -1,3 +1,13 @@
-class JobsService {}
+import { appState } from "../AppState.js";
+import { Job } from "../Models/Job.js";
+
+class JobsService {
+  async getJobs() {
+    const res = await axios.get("https://bcw-sandbox.herokuapp.com/api/jobs");
+    // console.log(res.data);
+    appState.jobs = res.data.map((j) => new Job(res.data));
+    // console.log(appState.jobs);
+  }
+}
 
 export const jobsService = new JobsService();
